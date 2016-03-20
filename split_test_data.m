36c7f67a05130e69f520be184961967233daa899
llicanisl <a.omerarol@gmail.com>
2016-03-20 16:28:23 +0200

    Split test and traning data

---
+++ split_test_data.m
@@ -0,0 +1,6 @@
+table = load_dataset('DATASETS');
+%%
+training_users = randperm(110000,70000)';
+
+training_data = table(ismember(table(:,1),training_users),:);
+test_data = table(~ismember(table(:,1),training_users),:);
