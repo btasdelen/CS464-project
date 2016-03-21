62ddb2c4d1aaf68bf4df36f6636897de06dd23ca
llicanisl <a.omerarol@gmail.com>
2016-03-22 00:16:51 +0200

    better factory

--- dataset_modify/big_matrix_factory.m
+++ dataset_modify/big_matrix_factory.m
@@ -1,6 +1,9 @@
-user_no = length(training_users);
+training_users = unique(training_data(:, 1));
 pointer_songs = unique(training_data(:, 2));
+
+user_no = length(training_users);
 song_no = length(pointer_songs);
+
 big_matrix = zeros(song_no, user_no);
 
 % for t = 1:user_no
@@ -13,3 +16,5 @@ big_matrix = zeros(song_no, user_no);
 for n=1:length(training_data)
     big_matrix(find(pointer_songs == training_data(n,2)),find(training_users == training_data(n,1))) = training_data(n,3);
 end
+
+% [labels,centers] = kmeans(big_matrix', 70)
