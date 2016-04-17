c03b047007b8aeb8e673ca035b43ab1cd1ff5554
tugyan <bilaltasdelen@windowslive.com>
2016-04-17 21:58:09 +0300

    Colloborative Filtering added (reverted from commit 95a9b516fe92232fc43519d342c2b338da511245)

--- dataset_modify/modify_dataset.py
+++ dataset_modify/modify_dataset.py
@@ -1,13 +1,8 @@
-import os
-import sys
-
-file_path = sys.argv[1]
-
 user = ''
-matrix_file = open(os.path.join(file_path ,'user_song_count_matrix.txt'), 'w')
+matrix_file = open('user_song_count_matrix.txt', 'w')
 songs_list = []
 song = ''
-with open(os.path.join(file_path ,'kaggle_visible_evaluation_triplets.txt'), 'r') as f:
+with open('kaggle_visible_evaluation_triplets.txt', 'r') as f:
     data = f.readlines()
     data = [section.split('\t') for section in data]
 
@@ -19,7 +14,7 @@ with open(os.path.join(file_path ,'kaggle_visible_evaluation_triplets.txt'), 'r'
         if line[1] not in songs_list:
             songs_list.append(line[1])
 
-        line[1] = str(songs_list.index(line[1]) + 1)
+        line[1] = str(songs_list.index(line[1]))
         line[0] = str(i)
         matrix_file.write(", ".join(line))
 
