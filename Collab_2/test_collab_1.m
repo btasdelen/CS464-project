8d30f1cef0067c9f3e81e1c8f77d0a638fe6ce96
llicanisl <a.omerarol@gmail.com>
2016-04-18 01:22:15 +0300

    Collab_filt mini_test Collab_filt çalışıyor gibi

--- Collab_2/test_collab_1.m
+++ Collab_2/test_collab_1.m
@@ -14,10 +14,30 @@ training_data = data_table(training_users(1:70000),:);
 test_data = data_table(training_users(70001:end),:);
 %%
 test_user = test_data(1,:);
+temp_test = test_user
 test_user = full(test_user);
+test_user(50000:end) = 0;
 
-similarity = zeros(70000,1);
+%%
 index = 1:song_no;
-for n = index(test_user==1)
-   similarity = similarity + training_data(:,n);
-end
+test_user_song = index(test_user==1);
+
+% similarity = zeros(70000,1);
+% index = 1:song_no;
+% for n = index(test_user==1)
+%    similarity = similarity + training_data(:,n);
+% end
+
+% Similarity without FOR
+similarity = sum(training_data(:,test_user==1),2);
+
+% Remove songs that already listened
+training_data(:,test_user_song) = [];
+index(test_user_song) = [];
+
+rank = training_data'*similarity;
+rank = full(rank);
+
+top_songs = sortrows([rank index']);
+top_songs = flipud(top_songs);
+top_songs(1:10,:)
