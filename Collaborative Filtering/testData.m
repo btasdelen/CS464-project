2e0acafa229cbd81995493ecab7c6382b28045ac
tugyan <bilaltasdelen@windowslive.com>
2016-04-17 22:21:01 +0300

    Colloborative Filtering added (reverted from commit 95a9b516fe92232fc43519d342c2b338da511245)

--- Collaborative Filtering/testData.m
+++ Collaborative Filtering/testData.m
@@ -1,4 +1,5 @@
 sparse_mat = eachMovieReader();
 numNonActive=size(sparse_mat,1)-20000;
 [activeMat,otherMat]=splitUsers(sparse_mat,20000,numNonActive);
-[pred,meanPred] = predictPreferencePD(activeMat , otherMat,numValues,sigma);
+%%
+[pred,meanPred] = predictPreferencePD( A, otherMat,10,1);
