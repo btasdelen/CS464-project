b6ae77e5380ed8598a7eb7e7a6691bed459ca021
tugyan <bilaltasdelen@windowslive.com>
2016-04-20 00:27:12 +0300

    Colloborative Filtering added (reverted from commit 95a9b516fe92232fc43519d342c2b338da511245) (reverted from commit af17cbf54bac65e0847d7ae14d5f73bc612c48e6) (reverted from commit a4b3817ad5ebfabc4e14ef65154560ba0054685a)

---
+++ dataset_modify/remove_non_active_users.m
@@ -0,0 +1,15 @@
+function [ active_users ] = remove_non_active_users( dataset, k )
+%REMOVE_NON_ACTIVE_USERS Summary of this function goes here
+%   Detailed explanation goes here
+
+    users = unique(dataset);
+    n = 0;
+    active_users = [];
+    for user=users'
+        if sum(dataset(dataset(:, 1) == user, 3)) > k
+           n = n + 1;
+           active_users = [active_users; dataset(dataset(:, 1) == user, :)];
+        end
+    end
+end
+
