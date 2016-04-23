e4704ba786b9c3c72c50b0c7d67b75ab4a7350de
tugyan <bilaltasdelen@windowslive.com>
2016-04-23 23:46:57 +0300

    e

--- dataset_modify/prepare_for_collab.m
+++ dataset_modify/prepare_for_collab.m
@@ -2,6 +2,7 @@
 mat = load('../DATASETS/user_song_count_matrix.txt');
 mat(:, 3) = 1; 
 data_table = remove_non_active_users(mat, 5);
+data_table = remove_non_active_songs(data_table, 5);
 clear mat;
 %%
 [train, test] = split_dataset(data_table, 1000);
