82f8eaf740ab9b00499c863ec202257517d84cad
llicanisl <a.omerarol@gmail.com>
2016-04-26 00:02:12 +0300

    cleaner data set

---
+++ cleandataset.m
@@ -0,0 +1,20 @@
+% prev = data_table(1,:);
+% curr = data_table(2,:);
+% 
+% for n=2:size(data_table,1)
+%     prev = data_table(n-1,:);
+%     curr = data_table(n,:);
+%     
+%     if curr(1)~=prev(1)
+%         data_table(n,1) = prev(1)+1;
+%     end
+%     
+% end
+
+songs = unique(new_data_table(:,2));
+k = 0;
+cleaner_data_table = new_data_table;
+for user=songs'
+    k=k+1;
+    cleaner_data_table(new_data_table(:,2) == user, 2) = k;
+end
